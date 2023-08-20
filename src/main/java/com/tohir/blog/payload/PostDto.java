19df94a130b854bf3b3e5cf99b167b906dd59077
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-21 00:27:03 +0600

    :pencil2: Add: PostDto class

---
+++ src/main/java/com/tohir/blog/payload/PostDto.java
@@ -0,0 +1,13 @@
+package com.tohir.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
