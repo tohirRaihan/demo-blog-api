d66a13cc8385e747ba835b60cbbec626392c1f0a
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-20 23:28:45 +0600

    :pencil2: Add: Post Entity

---
+++ src/main/java/com/tohir/blog/entity/Post.java
@@ -0,0 +1,31 @@
+package com.tohir.blog.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    private String description;
+    private String content;
+
+}
