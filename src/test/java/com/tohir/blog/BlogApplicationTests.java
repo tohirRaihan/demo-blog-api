21a57d739b189ee2f6e3699a73517b2aa84c7d69
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-20 22:03:17 +0600

    Initial Commit

---
+++ src/test/java/com/tohir/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tohir.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
