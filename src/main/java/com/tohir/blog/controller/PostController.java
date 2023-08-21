b51028813c2971b6cb3845b62024bd6ee18c7fc7
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-22 00:01:13 +0600

    :pencil2: Add: added createPost method in PostController class

--- src/main/java/com/tohir/blog/controller/PostController.java
+++ src/main/java/com/tohir/blog/controller/PostController.java
@@ -1,8 +1,26 @@
 package com.tohir.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tohir.blog.payload.PostDto;
+import com.tohir.blog.service.PostService;
+
 @RestController
+@RequestMapping("/api/v1/posts")
 public class PostController {
     
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
 }
