30c3937c99ea866f043cec5751693aeeffd67145
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-26 14:02:07 +0600

    :pencil2: Add: added CommentRepository interface

--- src/main/java/com/tohir/blog/entity/Post.java
+++ src/main/java/com/tohir/blog/entity/Post.java
@@ -1,10 +1,15 @@
 package com.tohir.blog.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,5 +32,8 @@ public class Post {
 
     private String description;
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 
 }
