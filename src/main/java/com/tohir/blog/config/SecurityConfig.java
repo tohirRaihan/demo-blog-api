c8a2f7ee2b421e1830c9251400b6be23ed8b06ff
Tohir Raihan <tohir.raihan@gmail.com>
2024-03-01 16:21:47 +0600

    :up: Update: impliment databae based authentication

--- src/main/java/com/tohir/blog/config/SecurityConfig.java
+++ src/main/java/com/tohir/blog/config/SecurityConfig.java
@@ -3,24 +3,34 @@ package com.tohir.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -34,19 +44,20 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails tohir = User.builder()
-                .username("tohir")
-                .password(passwordEncoder().encode("1234"))
-                .roles("USER")
-                .build();
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails tohir = User.builder()
+    //             .username("tohir")
+    //             .password(passwordEncoder().encode("1234"))
+    //             .roles("USER")
+    //             .build();
 
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("1234"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(tohir, admin);
-    }
+    //     UserDetails admin = User.builder()
+    //             .username("admin")
+    //             .password(passwordEncoder().encode("1234"))
+    //             .roles("ADMIN")
+    //             .build();
+    //     return new InMemoryUserDetailsManager(tohir, admin);
+    // }
+    
 }
