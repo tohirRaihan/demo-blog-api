355f5ddaaf608bee17b4f4650136285b86275c9a
Tohir Raihan <tohir.raihan@gmail.com>
2024-03-05 16:40:24 +0600

    :pencil2: Add: data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `users`(`id`, `email`, `name`, `password`, `username`) VALUES ('1','tohir@gmail.com','Tohir Raihan','$2a$10$EB7tmZrVyHcAn.oYBaedsOTxw.SElAm5QaH0QOFRdM/mpl82GUulW','tohir');
+INSERT INTO `users`(`id`, `email`, `name`, `password`, `username`) VALUES ('2','admin@gmail.com','Admin','$2a$10$PY6xqxELTkPOKh0l07L/Re.XDE4lxryYCpe8jMi1xvIdX8q9kO.RK','admin');
+
+INSERT INTO `roles`(`id`, `name`) VALUES ('1','ROLE_USER');
+INSERT INTO `roles`(`id`, `name`) VALUES ('2','ROLE_ADMIN');
+
+INSERT INTO `users_roles`(`role_id`, `user_id`) VALUES ('1','1');
+INSERT INTO `users_roles`(`role_id`, `user_id`) VALUES ('2','2');
