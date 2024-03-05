8c01cad4440045e3677a0fdd3031b0073ac11b36
Tohir Raihan <tohir.raihan@gmail.com>
2024-03-06 00:59:58 +0600

    :up: Update: small fixes for dockerizing application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- USE blog;
 INSERT INTO `users`(`id`, `email`, `name`, `password`, `username`) VALUES ('1','tohir@gmail.com','Tohir Raihan','$2a$10$EB7tmZrVyHcAn.oYBaedsOTxw.SElAm5QaH0QOFRdM/mpl82GUulW','tohir');
 INSERT INTO `users`(`id`, `email`, `name`, `password`, `username`) VALUES ('2','admin@gmail.com','Admin','$2a$10$PY6xqxELTkPOKh0l07L/Re.XDE4lxryYCpe8jMi1xvIdX8q9kO.RK','admin');
 
